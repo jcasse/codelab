5028ccc561a903eacf23b60630dfc35b1c2c79fd
Juan Casse <jcasse@beyond.ai>
2018-05-16 20:58:43 -0700

    Correctly lays out GoldenLayout objects side by side:
    - If the dimensions of td are given as percentages, the size of
      the contents of td will modify the size of td even if the
      contents do not fill the td. I tested this with a simple table
      and just text inside the tds.
      This problem is fixed by setting the table-layout to fixed:
      table-layout: fixed;
    - The following website says tables should not be used for layout:
      https://css-tricks.com/complete-guide-table-element/
    
    Problems:
    - Unhidding after hidding does not recover the table cell correctly
    
    Other:
    - Link in side bar (leftmost narrow table cell) can be rotated:
      https://stackoverflow.com/questions/15806925/how-to-rotate-text-left-90-degree-and-cell-size-is-adjusted-according-to-text-in

--- Web/Tabs/frontend-client/javascript/hide-tree.js
+++ Web/Tabs/frontend-client/javascript/hide-tree.js
@@ -1,29 +1,28 @@
-        // Wait for the page to load first
-        window.onload = function() {
+// Wait for the page to load first.
+window.onload = function() {
 
-          //Get a reference to the link on the page
-          // with an id of "mylink"
-          var a = document.getElementById("mylink");
+    // Get a reference to the link on the page
+    // with id.
+    var a = document.getElementById("hidetree");
 
-          //Set code to run when the link is clicked
-          // by assigning a function to "onclick"
-          a.onclick = function() {
+    // Set code to run when the link is clicked
+    // by assigning a function to "onclick".
+    a.onclick = function() {
 
-            // Your code here...
-    var x = document.getElementById("hideme");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-            //If you don't want the link to actually
-            // redirect the browser to another page,
-            // "google.com" in our example here, then
-            // return false at the end of this block.
-            // Note that this also prevents event bubbling,
-            // which is probably what we want here, but won't
-            // always be the case.
-            return false;
-          }
+        var x = document.getElementById("treeid");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
         }
+
+        // If you don't want the link to actually
+        // redirect the browser to another page,
+        // "google.com" in our example here, then
+        // return false at the end of this block.
+        // Note that this also prevents event bubbling,
+        // which is probably what we want here, but won't
+        // always be the case.
+        return false;
+    }
+}
